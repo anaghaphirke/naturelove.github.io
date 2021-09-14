ee5fa1c600171f8978cb0da4379d67f47cf533f4
anaghaphirke <90553930+anaghaphirke@users.noreply.github.com>
2021-09-14 22:27:33 +0530

    vue route is added, checking import/export

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//import Home from './home.js'
+import {Home} from './js/home.js'
+
+
+
+const Contact = { template: '<div>Contact page <p>this is Contact</p></div>' }
+
+const routes = [
+  { path: '/home', component: Home },
+  { path: '/contact', component: Contact }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+const app = new Vue({
+  router
+}).$mount('#app')
